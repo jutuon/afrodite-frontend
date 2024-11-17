e0c300f3579428e64a638750dcf7fe764cf2f05d
Juuso Tuononen <jutuon@outlook.com>
2024-11-17 11:44:47 +0200

    Release version 0.5

--- lib/ui_utils/dialog.dart
+++ lib/ui_utils/dialog.dart
@@ -18,7 +18,7 @@ void showAppAboutDialog(BuildContext context) {
     context: context,
     builder: (context) => AboutDialog(
       applicationName: context.strings.app_name,
-      applicationVersion: "0.4",
+      applicationVersion: "0.5",
       applicationIcon: Image.asset(
         ImageAsset.appLogo.path,
         width: ICON_SIZE,
