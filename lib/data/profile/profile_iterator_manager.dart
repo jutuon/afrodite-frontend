284ed7c170a829437ff2f4e60f1e55596ea863ec
Juuso Tuononen <jutuon@outlook.com>
2025-02-26 13:21:38 +0200

    Hide non-accepted primary profile images from profile grid

--- lib/data/profile/profile_iterator_manager.dart
+++ lib/data/profile/profile_iterator_manager.dart
@@ -138,7 +138,7 @@ class ProfileIteratorManager {
       for (final p in list) {
         final isBlocked = await chat.isInSentBlocks(p.uuid);
         final alreadyReturned = _duplicateAccountsPreventer.contains(p.uuid);
-        final invalidPrimaryContent = p.content.firstOrNull?.primary != true;
+        final invalidPrimaryContent = p.content.firstOrNull?.primary != true || p.content.firstOrNull?.accepted != true;
 
         if (isBlocked || alreadyReturned || p.uuid == currentUser || invalidPrimaryContent) {
           toBeRemoved.add(p);
